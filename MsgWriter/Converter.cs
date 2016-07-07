ad76c07671efe8c806ce1afe76c8ec2fbea3c925
sicos1977 <sicos2002@hotmail.com>
2016-07-07 19:28:47 +0200

    Added converter class with some methods

---
+++ MsgWriter/Converter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MsgWriter
+{
+    /// <summary>
+    ///     This class exposes some methods to convert MSG to EML and vice versa
+    /// </summary>
+    public static class Converter
+    {
+        /// <summary>
+        ///     Converts an EML file to MSG format
+        /// </summary>
+        /// <param name="emlFileName"></param>
+        /// <param name="msgFileName"></param>
+        public static void ConvertEmlToMsg(string emlFileName, string msgFileName)
+        {
+            
+        }
+
+        /// <summary>
+        ///     Converts an MSG file to EML format
+        /// </summary>
+        /// <param name="emlFileName"></param>
+        /// <param name="msgFileName"></param>
+        public static void ConvertMsgToEml(string emlFileName, string msgFileName)
+        {
+
+        }
+    }
+}
