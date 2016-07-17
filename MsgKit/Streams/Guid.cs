6bf7559908e5cfdac672074ec7d490517b527515
sicos1977 <sicos2002@hotmail.com>
2016-07-17 15:59:10 +0200

    Added empty Guid and String Stream classes

---
+++ MsgKit/Streams/Guid.cs
@@ -0,0 +1,16 @@
+﻿namespace MsgKit.Streams
+{
+    /// <summary>
+    ///     The GUID stream MUST be named "__substg1.0_00020102". It MUST store the property set GUID part of the property name
+    ///     of all named properties in the Message object or any of its subobjects, except for those named properties that have
+    ///     PS_MAPI or PS_PUBLIC_STRINGS, as described in [MS-OXPROPS] section 1.3.2, as their property set GUID. The GUIDs are 
+    ///     stored in the stream consecutively like an array. If there are multiple named properties that have the same property 
+    ///     set GUID, then the GUID is stored only once and all the named properties will refer to it by its index.
+    /// </summary>
+    /// <remarks>
+    ///     See https://msdn.microsoft.com/en-us/library/ee220039(v=exchg.80).aspx
+    /// </remarks>
+    internal sealed class Guid
+    {
+    }
+}
---
+++ MsgKit/Streams/String.cs
@@ -0,0 +1,17 @@
+﻿namespace MsgKit.Streams
+{
+    /// <summary>
+    ///     The string stream MUST be named "__substg1.0_00040102". It MUST consist of one entry for each string named
+    ///     property, and all entries MUST be arranged consecutively, like in an array. As specified in section 2.2.3.1.2, 
+    ///     the offset, in bytes, to use for a particular property is stored in the corresponding entry in the entry stream.
+    ///     That is a byte offset into the string stream from where the entry for the property can be read.The strings MUST 
+    ///     NOT be null-terminated. Implementers can add a terminating null character to the string after they read it from 
+    ///     the stream, if one is required by the implementer's programming language.
+    /// </summary>
+    /// <remarks>
+    ///     See https://msdn.microsoft.com/en-us/library/ee124409(v=exchg.80).aspx
+    /// </remarks>
+    internal sealed class String
+    {
+    }
+}
