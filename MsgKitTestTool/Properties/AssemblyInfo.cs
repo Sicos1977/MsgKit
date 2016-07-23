c5dbaeeed65dc8bf3803bf58c97f55400a6b3ce5
sicos1977 <sicos2002@hotmail.com>
2016-07-23 20:02:25 +0200

    Some more documentation added

--- MsgKitTestTool/Properties/AssemblyInfo.cs
+++ MsgKitTestTool/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MsgWriterTestTool")]
+[assembly: AssemblyTitle("MsgKitTestTool")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MsgWriterTestTool")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyProduct("MsgKitTestTool")]
+[assembly: AssemblyCopyright("Copyright ©  2015 - 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
