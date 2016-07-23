42949b4984b6c1830a7cbff0406b5951a9ec39d5
sicos1977 <sicos2002@hotmail.com>
2016-07-23 19:33:31 +0200

    Fixed copy paste errors

--- MsgKit/Streams/EmbeddedMessageProperties.cs
+++ MsgKit/Streams/EmbeddedMessageProperties.cs
@@ -1,7 +1,12 @@
 ﻿//
 // EmbeddedMessageProperties.cs
 //
-// Author: RecipientRowDisplayType and associated documentation files (the "Software"), to deal
+// Author: Kees van Spelde <sicos2002@hotmail.com>
+//
+// Copyright (c) 2015-2016 Magic-Sessions. (www.magic-sessions.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
