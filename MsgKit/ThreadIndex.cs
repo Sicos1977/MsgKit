bb739baddaa94ddf7ff78b962368b92e0d9ec39b
sicos1977 <sicos2002@hotmail.com>
2016-08-14 19:14:13 +0200

    Added thread index class

---
+++ MsgKit/ThreadIndex.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Linq;
+
+namespace MsgKit
+{
+    /// <summary>
+    ///     Identifies a particular conversation thread; computed from message references.
+    /// </summary>
+    public class ThreadIndex
+    {
+        #region Properties
+        /// <summary>
+        /// The date and time
+        /// </summary>
+        public DateTime Date { get; private set; }
+
+        /// <summary>
+        /// The unique GUID for this thread
+        /// </summary>
+        public Guid Id { get; private set; }
+
+        /// <summary>
+        /// The RAW base64 encoded thread index
+        /// </summary>
+        public string Raw { get; private set; }
+
+        /// <summary>
+        /// Returns <c>true</c> when the thread index is valid
+        /// </summary>
+        public bool IsValid
+        {
+            get { return Date != default(DateTime) && Id != default(Guid); }
+        }
+        #endregion
+
+        #region Constructor
+        /// <summary>
+        /// Creates this object and parses the given <paramref name="threadIndex"/>
+        /// </summary>
+        /// <param name="threadIndex"></param>
+        public ThreadIndex(string threadIndex)
+        {
+            Raw = threadIndex;
+
+            var bytes = Convert.FromBase64String(threadIndex);
+
+            // thread index length should be 22 plus extra 5 bytes per reply
+            if (bytes.Length < 22 || (bytes.Length - 22) % 5 != 0)
+                return;
+
+            Id = new Guid(bytes.Skip(6).Take(16).ToArray());
+
+            var headerTicks = bytes
+                .Take(6)
+                .Select(b => (long)b)
+                .Aggregate((l1, l2) => (l1 << 8) + l2)
+                << 16;
+
+            Date = new DateTime(headerTicks, DateTimeKind.Utc).AddYears(1600);
+
+            var childBlockCount = (bytes.Length - 22) / 5;
+
+            for (var i = 0; i < childBlockCount; i++)
+            {
+                var childTicks = bytes
+                    .Skip(22 + i * 5).Take(4)
+                    .Select(b => (long)b)
+                    .Aggregate((l1, l2) => (l1 << 8) + l2)
+                    << 18;
+
+                childTicks &= ~((long)1 << 50);
+                Date = Date.AddTicks(childTicks);
+            }
+        }
+        #endregion
+
+        #region ToString
+        /// <summary>
+        /// Returns the information about this thread index as a string
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return string.Format("Id: {0}, Date: {1}", Id, Date.ToLocalTime());
+        }
+        #endregion
+    }
+}
