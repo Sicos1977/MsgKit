608625199a9d0321afca9e4446b03d223c6e87d7
Sicos1977 <sicos2002@hotmail.com>
2021-03-31 19:21:43 +0200

    Update copyright info

--- MsgKit/PropertyTags.cs
+++ MsgKit/PropertyTags.cs
@@ -4,7 +4,7 @@
 //
 // Author: Kees van Spelde <sicos2002@hotmail.com>
 //
-// Copyright (c) 2015-2020 Magic-Sessions. (www.magic-sessions.com)
+// Copyright (c) 2015-2021 Magic-Sessions. (www.magic-sessions.com)
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
