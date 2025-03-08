969cb25f4136e11f708eeab96cabe93e97af2aa4
James John McGuire <jamesjohnmcguire@gmail.com>
2025-03-08 20:43:44 +0900

    Remove Setting SentOn = DateTime.UtcNow
    It is better to leave this not set rather then setting the time to now,
    as it is too assuming about the time of the message.  I came across
    this processing emails from 12 years ago and they were showing up as
    being created today.  So, setting the time to now, would be incorrect
    and just confusing.  Better to leave it not set, as it will indicate
    incomplete information.

--- MsgKit/Email.cs
+++ MsgKit/Email.cs
@@ -478,8 +478,6 @@ public class Email : Message, IDisposable
         if (MessageEditorFormat != MessageEditorFormat.EDITOR_FORMAT_DONTKNOW)
             TopLevelProperties.AddProperty(PropertyTags.PR_MSG_EDITOR_FORMAT, MessageEditorFormat);
 
-        SentOn ??= DateTime.UtcNow;
-
         if (ReceivedOn.HasValue && ReceivedOn > DateTime.MinValue)
             TopLevelProperties.AddProperty(PropertyTags.PR_MESSAGE_DELIVERY_TIME, ReceivedOn.Value.ToUniversalTime());
 
